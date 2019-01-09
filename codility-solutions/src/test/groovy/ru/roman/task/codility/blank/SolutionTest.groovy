c5398f76b3cda4183dd18f068057b55abb96be37
rrrrrr111 <curdes@gmail.com>
2019-01-09 13:03:37 +0300

    Rotation task

--- codility-solutions/src/test/groovy/ru/roman/task/codility/blank/SolutionTest.groovy
+++ codility-solutions/src/test/groovy/ru/roman/task/codility/blank/SolutionTest.groovy
@@ -2,6 +2,7 @@ package ru.roman.task.codility.blank
 
 import groovy.transform.CompileStatic
 import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
 
 @CompileStatic
 //@Test
