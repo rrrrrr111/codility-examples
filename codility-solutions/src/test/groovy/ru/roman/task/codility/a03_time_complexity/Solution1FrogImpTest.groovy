740b081a1ece0133651ca288b5d6dbcc63515778
rrrrrr111 <curdes@gmail.com>
2019-01-10 17:19:25 +0300

    Frog jump task

--- codility-solutions/src/test/groovy/ru/roman/task/codility/a03_time_complexity/Solution1FrogImpTest.groovy
+++ codility-solutions/src/test/groovy/ru/roman/task/codility/a03_time_complexity/Solution1FrogImpTest.groovy
@@ -12,15 +12,19 @@ class Solution1FrogImpTest {
     @DataProvider
     private Object[][] provider() {
         [
-                [0, 0],
+                [0, 0, 0, 0],
+                [1, 2, 1, 1],
+                [0, 2, 1, 2],
+                [10, 20, 3, 4],
+                [10, 85, 30, 3],
 
         ] as Object[][]
     }
 
     @Test(dataProvider = "provider")
-    void test(int value, int expected) {
+    void test(int value1, int value2, int value3, int expected) {
 
-        def actual = subj.solution(value)
+        def actual = subj.solution(value1, value2, value3)
         assert actual == expected
     }
 }
