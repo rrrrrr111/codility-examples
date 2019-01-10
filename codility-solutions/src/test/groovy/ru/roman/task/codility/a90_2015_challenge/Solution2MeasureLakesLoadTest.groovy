d8a19594361b4fa77e6ea37b3e406fe2a627102a
rrrrrr111 <curdes@gmail.com>
2019-01-10 11:43:07 +0300

    Measure Lakes task, load test

--- codility-solutions/src/test/groovy/ru/roman/task/codility/a90_2015_challenge/Solution2MeasureLakesLoadTest.groovy
+++ codility-solutions/src/test/groovy/ru/roman/task/codility/a90_2015_challenge/Solution2MeasureLakesLoadTest.groovy
@@ -1,50 +1,57 @@
 package ru.roman.task.codility.a90_2015_challenge
 
 import groovy.transform.CompileStatic
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 
 @CompileStatic
 @Test
 class Solution2MeasureLakesLoadTest {
-    private Solution2MeasureLakes subj = new Solution2MeasureLakes()
+    private final Solution2MeasureLakes subj = new Solution2MeasureLakes()
+
+    static int[] list
+
+    @BeforeClass
+    static void setUp() {
+        final int size = 100_000
+        final int half = size.intdiv(2).intValue()
+        list = new int[size]
+
+        final int max = 100_000_000
+        final int step = 1_000
+
+        int val = max
+        boolean turn = true
+
+
+        for (int i = 0; i < list.length; i++) {
+            if (i >= half) {
+                if (turn) {
+                    val = val + step
+                } else {
+                    val = val - step.intdiv(2)
+                }
+            } else {
+                if (turn) {
+                    val = val - step
+                } else {
+                    val = val + step.intdiv(2)
+                }
+            }
+            list[i] = val
+            turn = !turn
+        }
+    }
 
     @DataProvider
     private Object[][] provider() {
-        final def size = 1_000_000
-        final int[] list = new int[size]
-
-        final def random = new Random()
-        final int step = 1000
-
-        int prev = 0
-        int val = 0
-        boolean turn = true
-
-        final int max = 100_000_000
-        for (int i = 0; i < list.length; i++) {
-            if (turn) {
-                val = val + step
-            } else {
-                val = val - step.intdiv(2)
-            }
-
-            list[i] = val
-            prev = val
-            turn = !turn
-        }
-        return [
-                [list, 988],
+        [
+                [list, 12500000],
         ] as Object[][]
     }
 
-    @Test
-    void test1() {
-
-
-    }
-
-    @Test(dataProvider = "provider")
+    @Test(dataProvider = "provider", timeOut = 3_000L)
     void test(int[] value, int expected) {
 
         def actual = subj.solution(value)
