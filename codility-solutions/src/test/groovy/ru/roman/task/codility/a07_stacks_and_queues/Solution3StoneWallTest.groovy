2e01cb34321c97e90cfad4331b6bb1e43a84d719
rrrrrr111 <curdes@gmail.com>
2019-01-16 17:40:27 +0300

    StoneWall task

---
+++ codility-solutions/src/test/groovy/ru/roman/task/codility/a07_stacks_and_queues/Solution3StoneWallTest.groovy
@@ -0,0 +1,25 @@
+package ru.roman.task.codility.a07_stacks_and_queues
+
+import groovy.transform.CompileStatic
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+@CompileStatic
+@Test
+class Solution3StoneWallTest {
+    private Solution3StoneWall subj = new Solution3StoneWall()
+
+    @DataProvider
+    private Object[][] provider() {
+        [
+                [[8, 8, 5, 7, 9, 8, 7, 4, 8] as int[], 7],
+        ] as Object[][]
+    }
+
+    @Test(dataProvider = "provider")
+    void test(int[] value, int expected) {
+
+        def actual = subj.solution(value)
+        assert actual == expected
+    }
+}
