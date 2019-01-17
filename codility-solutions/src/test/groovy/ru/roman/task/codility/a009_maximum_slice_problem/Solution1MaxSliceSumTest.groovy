7480b773d0fcb7d36dad8c1639889317b313e379
rrrrrr111 <curdes@gmail.com>
2019-01-17 11:39:05 +0300

    MaxSliceSum task

---
+++ codility-solutions/src/test/groovy/ru/roman/task/codility/a009_maximum_slice_problem/Solution1MaxSliceSumTest.groovy
@@ -0,0 +1,30 @@
+package ru.roman.task.codility.a009_maximum_slice_problem
+
+import groovy.transform.CompileStatic
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+@CompileStatic
+@Test
+class Solution1MaxSliceSumTest {
+    private Solution1MaxSliceSum subj = new Solution1MaxSliceSum()
+
+    @DataProvider
+    private Object[][] provider() {
+        [
+                [[0] as int[], 0],
+                [[1] as int[], 1],
+                [[1, 1] as int[], 2],
+                [[-1_000_000, 0, 1_000_000] as int[], 1_000_000],
+                [[1, 2, 3, 4, 5] as int[], 15],
+                [[3, 2, -6, 4, 0] as int[], 5],
+        ] as Object[][]
+    }
+
+    @Test(dataProvider = "provider")
+    void test(int[] value, int expected) {
+
+        def actual = subj.solution(value)
+        assert actual == expected
+    }
+}
