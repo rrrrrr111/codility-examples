f59b34ad8ff7593a87b9811f114f4cf527448164
rrrrrr111 <curdes@gmail.com>
2019-01-25 11:58:37 +0300

    FibonacciNumbersExample

--- java-study/src/main/java/ru/roman/samples/array/SieveOfEratosthenesExample.java
+++ java-study/src/main/java/ru/roman/samples/array/SieveOfEratosthenesExample.java
@@ -3,6 +3,8 @@ package ru.roman.samples.array;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+import static org.apache.commons.lang3.StringUtils.leftPad;
+
 /**
  *
  */
@@ -11,21 +13,21 @@ public class SieveOfEratosthenesExample {
     public static void main(String[] args) {
 
         int maxValue = 1_000;
-        int[] factors = prepareFactors(maxValue);
+        int[] factors = preparePrimeFactors(maxValue);
 
         System.out.printf("Nums    : %s%n",
                 Arrays.toString(
-                        IntStream.range(0, maxValue + 1).mapToObj(SieveOfEratosthenesExample::adjust).toArray()
+                        IntStream.range(0, maxValue + 1).mapToObj(v -> leftPad("" + v, 3)).toArray()
                 )
         );
         System.out.printf("Factors : %s%n",
                 Arrays.toString(
-                        IntStream.of(factors).mapToObj(SieveOfEratosthenesExample::adjust).toArray()
+                        IntStream.of(factors).mapToObj(v -> leftPad("" + v, 3)).toArray()
                 )
         );
     }
 
-    private static int[] prepareFactors(int maxValue) {
+    private static int[] preparePrimeFactors(int maxValue) {
         int max = maxValue + 1;
         int[] factors = new int[max];
 
@@ -40,12 +42,4 @@ public class SieveOfEratosthenesExample {
         return factors;
     }
 
-    private static String adjust(int value) {
-        return value > 9
-                ? (
-                value > 99
-                        ? "" + value
-                        : " " + value)
-                : "  " + value;
-    }
 }
