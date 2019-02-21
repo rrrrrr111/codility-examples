849d883decc188e50e6ebf83320265223ad03a5a
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-02-21 14:48:31 +0300

    operator examples

--- java-study/src/main/java/ru/roman/certification/operator/Combination.java
+++ java-study/src/main/java/ru/roman/certification/operator/Combination.java
@@ -14,18 +14,33 @@ public class Combination {
 
     public static void main(String[] args) {
 
-        int x = 1, b = 2;
+        int x = 1, y = 2;
 
-        x += b;  // x = x + b
-        x -= b;  // x = x - b
-        x *= b;   // x = x * b
-        x /= b;   // x = x / b
-        x %= b;   // x = x % b
-        x &= b;   // x = x & b
-        x |= b;   // x = x | b
-        x ^= b;   // x = x ^ b
-        x <<= b;  // x = x << b
-        x >>= b;  // x = x >> b
-        x >>>= b; // x = x >>> b
+        x += y;  // x = (int)(x + y)
+        x -= y;  // x = (int)(x - y)
+        x *= y;   // x = (int)(x * y)
+        x /= y;   // x = (int)(x / y)
+        x %= y;   // x = (int)(x % y)
+        x &= y;   // x = (int)(x & y)
+        x |= y;   // x = (int)(x | y)
+        x ^= y;   // x = (int)(x ^ y)
+        x <<= y;  // x = (int)(x << y)
+        x >>= y;  // x = (int)(x >> y)
+        x >>>= y; // x = (int)(x >>> y)
+
+        boolean r09 = true;
+        //boolean r10 != r09;     // illegal, not equals is not assignment
+        boolean r11 =! r09;     // false кривое форматирование
+
+        byte b1 = 127;
+        b1 += 0x7fffffff;              // 126 - вычисления по прежнему выполняются в int, и тут явно overflow
+                                       // однако assignment делают неявное приведение типа, явное приведение тут некуда вставиить
+        b1 += 0x7fffffffffffffffL;     // 126
+        b1 += 3.4E307D;                // -1
+
+        float f1 = 3.4E38f;
+        f1 *= 3.4E307d;                // Infinity
+
+        System.out.println(f1);
     }
 }
--- java-study/src/main/java/ru/roman/certification/operator/Unary.java
+++ java-study/src/main/java/ru/roman/certification/operator/Unary.java
@@ -9,13 +9,13 @@ package ru.roman.certification.operator;
  * 2.
  * 3.  b = 00000000 00000000 00000000 00001111
  * с = 11111111 11111111 11111111 11110000
- *
+ * <p>
  * 4. префиксная и постфиксная запись инкремента и дектремента имеет отличия
  * 5. Следует отметить, что в Java, в отличие от языка C, операции декремента и
  * инкремента могут применяться и к вещественным переменным (типа float и double).
- *
+ * <p>
  * 6. Дектремент и инкремент применяется ко всем числовым типам
- *
+ * <p>
  * 7. ~ оператор побитового инвертирования
  *
  * @author дтоь 13.03.2011
@@ -24,33 +24,24 @@ public class Unary {
 
     public static void main(String[] args) {
 
-        int a = -1, b = 15, c, d = 2;
-        short e = 2;
-        long f = 2;
-        char g = 2;
-        byte h = 2;
-        float i = 2;
-        double j = 2;
+        byte r00 = ~1;         // -2 - 00000001 -> 11111110
+        int r01 = ~1;          // -2 - 00000000000000000000000000000001 -> 11111111111111111111111111111110
+        int r02 = +(-1);       // -1
+        int r03 = -(-1);       // 1
+        float r04 = -(-1f);    // 1.0
+        //float r05 = ~1f;       // illegal
+        int r06 = -0x80000000;   // -2147483648
+        int r07 = -(-0x80000000);   // -2147483648 - min int - уникальный кейс, все integral аналогично
 
-        System.out.println(~-1);
-        System.out.println(-(-1));
-        System.out.println(+(-1));
+        //int r08 = --1;   // illegal
+        int min = 0x80000000;
+        ++min;                   // -2147483647
+        --min;                   // 2147483647 overflow
+        float minF = -3.4e38f;
+        --minF;                  // -3.4E38 - по идее overflow, однако из-за дискретности округляется к изначальному
 
-        System.out.println("~-2 =>" + ~-2 + "  ~-1 =>" + ~-1 + "  ~0 =>" + ~0 + "  ~1 =>" + ~1 + "  ~2 =>" + ~2);
-        System.out.println(-a);
-        System.out.println(+a);
+        boolean r09 = !true;     // false
 
-        System.out.println(b + " " + (c = ~b));
-
-        System.out.println(++d);
-        System.out.println(e++);
-
-        System.out.println(--f);
-        System.out.println(g--);
-        System.out.println(h--);
-        System.out.println(--i);
-        System.out.println(j--);
-
-
+        System.out.println(">>> " + minF);
     }
 }
