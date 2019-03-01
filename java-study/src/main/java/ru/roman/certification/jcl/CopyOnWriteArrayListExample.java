a28e2561359ea4a76dceb83ee4e75d4fda9f58be
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-01 20:52:10 +0300

    CopyOnWriteArrayList examples

---
+++ java-study/src/main/java/ru/roman/certification/jcl/CopyOnWriteArrayListExample.java
@@ -0,0 +1,24 @@
+package ru.roman.certification.jcl;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ *
+ */
+public class CopyOnWriteArrayListExample {
+
+
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        System.out.printf("Initial list %s%n", list);
+
+        for (Integer i : list) {
+            list.add(3, 33);
+
+            System.out.printf("After mutation %s%n", list);
+        }
+
+        System.out.printf("Final %s%n", list);
+    }
+}
