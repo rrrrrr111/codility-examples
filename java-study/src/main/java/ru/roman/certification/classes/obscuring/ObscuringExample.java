3535b27a2f9a62dd55a0d78f3242c4f8fe5eaa3c
rrrrrr111 <curdes@gmail.com>
2019-03-04 16:58:10 +0300

    obscuring example

---
+++ java-study/src/main/java/ru/roman/certification/classes/obscuring/ObscuringExample.java
@@ -0,0 +1,21 @@
+package ru.roman.certification.classes.obscuring;
+
+import java.lang.String;     // obscured by type declared bellow
+import java.lang.*;
+
+public class ObscuringExample {
+    Integer String;
+
+    void foo() {
+        // new String("");    // illegal, java.lang.String obscured
+        new String();
+
+        String.valueOf(123);  // java.lang.String type obscured by variable String это не static метод
+                              // String а static метод Integer вызванный на переменной String типа Integer
+                              // если переменную убрать, нескомпилится, т к у типа ниже нет такого метода
+    }
+    class String {}           // ok, nested можно и с импортом даже
+}
+
+//class String {} // illegal т к если есть import, нельзя объявить top-level,
+                  // но можно если только импорт всего пакета import java.lang.*;
---
+++ java-study/src/main/java/ru/roman/certification/classes/obscuring/Vector.java
@@ -0,0 +1,13 @@
+package ru.roman.certification.classes.obscuring;
+
+import java.lang.*;
+import java.util.*;
+
+
+class Vector {
+
+    void foo() {
+
+        new Vector();
+    }
+}
