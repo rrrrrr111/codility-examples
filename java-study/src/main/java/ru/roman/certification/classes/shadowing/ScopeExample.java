b79a7084c0987bf8093c9af6a7fac3c155cf49bf
rrrrrr111 <curdes@gmail.com>
2019-03-04 15:06:25 +0300

    scope example

--- java-study/src/main/java/ru/roman/certification/classes/shadowing/ScopeExample.java
+++ java-study/src/main/java/ru/roman/certification/classes/shadowing/ScopeExample.java
@@ -2,10 +2,7 @@ package ru.roman.certification.classes.shadowing;
 
 class ScopeExample {}
 
-class Foo {
-    void f2(String s) {}
-}
-
+class Foo { void f2(String s) {} }
 class Test {
     void f1(int i) {}
     void f2(int i) {}
@@ -14,7 +11,7 @@ class Test {
         new Foo() {{                   // блок инициализации в анонимном классе
                 f1(0);
                 //f2(0);               // illegal, свой метод приоритетнее
-                f2("");             // не счетается shadowing'ом, т к оба метода при декларации не пересекаются по соупу
+                f2("");             // не счетается shadowing'ом, т к оба метода при декларации не пересекаются по скоупу
                 Test.this.f2(0);
             }};
     }
