5db7d004b7353264bfe19977bfb69417dbb1a63b
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-05 18:31:29 +0300

    Indirect Access examples

--- java-study/src/main/java/ru/roman/certification/classes/access/IndirectAccessClass.java
+++ java-study/src/main/java/ru/roman/certification/classes/access/IndirectAccessClass.java
@@ -5,12 +5,13 @@ import ru.roman.certification.classes.access.pkgbar.AllAccessTypesFoo;
 class IndirectAccessClass extends AllAccessTypesFoo.IndirectAccessor {
 
     IndirectAccessClass() {
-        new AllAccessTypesFoo().super();
+        new AllAccessTypesFoo().super();    // вызов конструктора родительского inner класса c передачей ему ссылки
+                                            // на его outer класс
     }
 
     @Override
     protected String fo() {
-        return super.fo();
+        return super.fo();                 // тут может быть indirect доступ к приватным полям инстанса outer класса
     }
 
     public static void main(String[] args) {
