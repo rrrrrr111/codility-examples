1c4d493c1918285a40e2ed59cd414294037b2f16
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-05 22:11:41 +0300

    annotation examples

--- java-study/src/main/java/ru/roman/certification/classes/annotation/AnnotationExample.java
+++ java-study/src/main/java/ru/roman/certification/classes/annotation/AnnotationExample.java
@@ -1,6 +1,7 @@
 package ru.roman.certification.classes.annotation;
 
 import java.lang.annotation.Documented;
+import java.util.List;
 
 @Documented
 @interface AnnotationExample {                   // тип аннотации по сути форма интерфейса
@@ -11,6 +12,29 @@ import java.lang.annotation.Documented;
     String lastModifiedBy() default "N/A";
     // Note use of array
     String[] reviewers();
+
+    Foo fa();
+    Foo[] ff();
+
+    Class<List>[] jjj() default List.class;  // массим из одного элемента можно убрать скобки
+    //Class<List<? extends Comparable<?>>>[] jgjgjg() default new Class<?>[0]; // illegal, attribute value must be constant
+    Class<List<? extends Comparable<?>>>[] jgjggjg() default {};
+
+    Object[] CONST = new Object[0];
+    //{   var b = "";}                // illegal
+
+    static class Jububu{}
+    static enum  Kakoo{}
+    static interface Mamou{}
 }
 
+
+enum Foo {}
+class Bazz {
+
+    void foo() {
+
+        Class[] dd =  {List.class};  // массим из одного элемента можно схлопывать
+    }
+}
 
