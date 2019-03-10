1859f66b27fefd89a724b5bb3d08acbebea4d7b8
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-10 04:31:50 +0300

    clinit examples

--- java-study/src/main/java/ru/roman/certification/classes/constructor/StaticInitException.java
+++ java-study/src/main/java/ru/roman/certification/classes/constructor/StaticInitException.java
@@ -1,17 +1,53 @@
 package ru.roman.certification.classes.constructor;
 
+import java.io.IOException;
+
 class StaticInitException {
+
+    public static void main(String[] args) throws Exception {
+        try {
+            new StaticInitException();
+        } catch (Throwable e) {
+            System.out.println("Got it: " + e);
+            throw e;
+        }
+    }
+
+    //private static final int HHH1 = foo();      // illegal
+    private int hh1 = foo();
+    private static final int HHH = foo1();
+    private int hh = foo1();
+    //private StaticInitException internalInstance = new StaticInitException(); // StackOverflowError
+
+    Fgoo fgoo = new Fgoo();
+
+    private StaticInitException() throws Exception {
+    }
+
+    {
+        //throw new InterruptedException();    // illegal
+        if (true) throw new InterruptedException();
+    }
+
+    public static int foo() throws InterruptedException {
+        return 0;
+    }
+
+    public static int foo1() throws RuntimeException {
+        return 0;
+    }
+}
+
+class Fgoo {
     static {
         //throw new RuntimeException();             // illegal
 
         if (true) try {
             throw new Exception();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (Throwable e) {
+            //throw new RuntimeException(e);
         }
         if (true) throw new RuntimeException();
-    }
-
-    public static void main(String[] args) {
+        //if (true) throw new IOException();       // illegal
     }
 }
