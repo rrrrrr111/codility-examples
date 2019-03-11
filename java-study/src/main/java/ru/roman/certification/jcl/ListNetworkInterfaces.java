7c21aa6e16c84a351b8f9a3d3bdc2ec25a93fd7e
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-12 02:19:34 +0300

    NetworkInterface examples

--- java-study/src/main/java/ru/roman/certification/jcl/ListNetworkInterfaces.java
+++ java-study/src/main/java/ru/roman/certification/jcl/ListNetworkInterfaces.java
@@ -1,7 +1,9 @@
 package ru.roman.certification.jcl;
 
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
 
@@ -18,15 +20,35 @@ public class ListNetworkInterfaces {
 
             out.printf("Display name: %s\n", netIf.getDisplayName());
             out.printf("Name: %s\n", netIf.getName());
+            printAddresses(netIf);
+            printProps(netIf);
 
             Enumeration<NetworkInterface> subIfs = netIf.getSubInterfaces();
             for (NetworkInterface subIf : Collections.list(subIfs)) {
 
                 out.printf("\tSub Interface Display name: %s\n", subIf.getDisplayName());
                 out.printf("\tSub Interface Name: %s\n", subIf.getName());
-
+                printAddresses(subIf);
+                printProps(subIf);
             }
             out.println();
         }
+    }
+
+    private static void printAddresses(NetworkInterface netIf) {
+        Enumeration<InetAddress> inetAddresses = netIf.getInetAddresses();
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            out.printf("InetAddress: %s\n", inetAddress);
+        }
+    }
+
+    private static void printProps(NetworkInterface netint) throws SocketException {
+        out.printf("Up? %s\n", netint.isUp());
+        out.printf("Loopback? %s\n", netint.isLoopback());
+        out.printf("PointToPoint? %s\n", netint.isPointToPoint());
+        out.printf("Supports multicast? %s\n", netint.supportsMulticast());
+        out.printf("Virtual? %s\n", netint.isVirtual());
+        out.printf("Hardware address: %s\n", Arrays.toString(netint.getHardwareAddress()));
+        out.printf("MTU: %s\n", netint.getMTU());
     }
 }
