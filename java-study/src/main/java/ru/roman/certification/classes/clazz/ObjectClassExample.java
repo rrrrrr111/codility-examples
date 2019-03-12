3ec548e3fccb15dde781225f7f9c1be33f6d7a28
rrrrrr111 <curdes@gmail.com>
2019-03-12 16:27:40 +0300

    class example

--- java-study/src/main/java/ru/roman/certification/classes/clazz/ObjectClassExample.java
+++ java-study/src/main/java/ru/roman/certification/classes/clazz/ObjectClassExample.java
@@ -1,20 +1,28 @@
 package ru.roman.certification.classes.clazz;
 
 import java.lang.reflect.Array;
+import java.lang.Long;
 
 /**
  *
  */
-class ObjectClassExample {
+public class ObjectClassExample {
 
     public static void main(String[] args) throws Exception {
 
         System.out.println(String.class);
-        System.out.println(int.class);
-        System.out.println(void.class);
         System.out.println(Class.forName("java.lang.Long"));
         System.out.println(Integer.valueOf(1).getClass());
+        System.out.println();
 
+        //System.out.println(Class.forName("Long"));                      // ClassNotFoundException
+        System.out.println(Integer.TYPE);
+        System.out.println(int.class);
+        System.out.println(void.class);
+        System.out.println(Class.class == Class.class.getClass());
+        System.out.println(Class.class == Class.class.getClass().getClass());
+        //System.out.println(Integer.class == Integer.class.getClass());       // illegal
+        System.out.println();
 
         var v1 = new Integer[]{1} instanceof Number[];                        // true - Контроль типов c учетом наследования
         var v2 = new Integer[]{1}.getClass() == new Integer[]{}.getClass();   // false - будет проверено четко совпадают ли классы
