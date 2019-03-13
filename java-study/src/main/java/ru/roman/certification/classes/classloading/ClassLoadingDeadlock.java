2c0acd0b26551e43d9a134a08aaed5401bb34625
rrrrrr111 <curdes@gmail.com>
2019-03-13 18:39:18 +0300

    Class Loading Deadlock example

---
+++ java-study/src/main/java/ru/roman/certification/classes/classloading/ClassLoadingDeadlock.java
@@ -0,0 +1,26 @@
+package ru.roman.certification.classes.classloading;
+
+public class ClassLoadingDeadlock {
+
+    public static void main(String[] args) {
+        Foo.foo(1);
+    }
+}
+
+
+class Foo {
+    static {
+        Thread t = new Thread(() -> foo(2));
+        t.setName("Manana");
+        t.start();
+
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static void foo(int i) {
+        System.out.println(Thread.currentThread().getName() + " >> " + i);
+    }
+}
