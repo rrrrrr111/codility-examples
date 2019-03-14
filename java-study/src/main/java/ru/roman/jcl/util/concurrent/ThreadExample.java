79e0ea16820aef5dca4d1a9994661b64bd72ed60
rrrrrr111 <curdes@gmail.com>
2019-03-14 17:40:45 +0300

    Thread example

---
+++ java-study/src/main/java/ru/roman/jcl/util/concurrent/ThreadExample.java
@@ -0,0 +1,16 @@
+package ru.roman.jcl.util.concurrent;
+
+public class ThreadExample {
+
+    public static void main(String[] args) {
+        var t = new Thread() {
+            public void run() {
+                pong();
+                //sleep();                   // illegal попытка вызвать метод из outer класса, т к тред зашадовил его своим методом
+            }                               // кроме того sleep() кидает checked InterruptedException
+        };
+        t.run();                            // выполняет тред, а не запускает его
+    }
+    static void pong() {}
+    static void sleep() {}
+}
