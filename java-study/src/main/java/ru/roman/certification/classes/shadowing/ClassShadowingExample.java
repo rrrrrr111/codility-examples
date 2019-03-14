ea0c1caa474a5b9cbf5344d852bffd8ccc4417fa
rrrrrr111 <curdes@gmail.com>
2019-03-14 14:31:22 +0300

    shadowing example

---
+++ java-study/src/main/java/ru/roman/certification/classes/shadowing/ClassShadowingExample.java
@@ -0,0 +1,10 @@
+package ru.roman.certification.classes.shadowing;
+
+public class ClassShadowingExample {
+}
+
+class StrungOut {
+    public static void main(String[] args) {    // это не main метод
+    }
+}
+class String {}
