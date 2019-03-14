f9338bfe9c77f945c1177e230ec05a9569982f11
rrrrrr111 <curdes@gmail.com>
2019-03-14 14:55:01 +0300

    obscuring example

--- java-study/src/main/java/ru/roman/certification/classes/obscuring/ClassObscuringExample.java
+++ java-study/src/main/java/ru/roman/certification/classes/obscuring/ClassObscuringExample.java
@@ -4,16 +4,27 @@ import ru.roman.certification.classes.obscuring.X.Y;
 
 class X {
 
-    static class Y { static String Z = "Black"; }
+    static class Y {
+        static String Z = "Black";
+    }
+
     static C Y = new C();
 }
-class C {    String Z = "White"; }
+
+class C {
+    String Z = "White";
+}
 
 class ShadesOfGray {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        new ShadesOfGray().foo();
+    }
+
+    <T extends X.Y> void foo() {
         System.out.println(X.Y.Z);
         System.out.println(Y.Z);
-        System.out.println(((X.Y)null).Z);
+        System.out.println(((X.Y) null).Z);
+        System.out.println(T.Z);
     }
 }
 
