7bab03426162783402861412bb66482bda5c60bd
rrrrrr111 <curdes@gmail.com>
2019-03-15 18:57:38 +0300

    codility task fixes

--- codility-solutions/src/main/java/ru/roman/task/codility/a006_sorting/Solution1Distinct.java
+++ codility-solutions/src/main/java/ru/roman/task/codility/a006_sorting/Solution1Distinct.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 
 /**
  * Write a function
- *
+ * <p>
  * class Solution { public int solution(int[] A); }
- *
+ * <p>
  * that, given an array A consisting of N integers, returns the number of distinct values in array A.
- *
+ * <p>
  * For example, given array A consisting of six elements such that:
- *
- *  A[0] = 2    A[1] = 1    A[2] = 1
- *  A[3] = 2    A[4] = 3    A[5] = 1
+ * <p>
+ * A[0] = 2    A[1] = 1    A[2] = 1
+ * A[3] = 2    A[4] = 3    A[5] = 1
  * the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
- *
+ * <p>
  * Write an efficient algorithm for the following assumptions:
- *
+ * <p>
  * N is an integer within the range [0..100,000];
  * each element of array A is an integer within the range [−1,000,000..1,000,000].
  */
--- codility-solutions/src/main/java/ru/roman/task/codility/a006_sorting/Solution2MaxProductOfThree.java
+++ codility-solutions/src/main/java/ru/roman/task/codility/a006_sorting/Solution2MaxProductOfThree.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 /**
  * A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
- *
+ * <p>
  * For example, array A such that:
- *
+ * <p>
  * A[0] = -3
  * A[1] = 1
  * A[2] = 2
@@ -14,20 +14,20 @@ import java.util.Arrays;
  * A[4] = 5
  * A[5] = 6
  * contains the following example triplets:
- *
+ * <p>
  * (0, 1, 2), product is −3 * 1 * 2 = −6
  * (1, 2, 4), product is 1 * 2 * 5 = 10
  * (2, 4, 5), product is 2 * 5 * 6 = 60
  * Your goal is to find the maximal product of any triplet.
- *
+ * <p>
  * Write a function:
- *
+ * <p>
  * class Solution { public int solution(int[] A); }
- *
+ * <p>
  * that, given a non-empty array A, returns the value of the maximal product of any triplet.
- *
+ * <p>
  * For example, given array A such that:
- *
+ * <p>
  * A[0] = -3
  * A[1] = 1
  * A[2] = 2
@@ -35,9 +35,9 @@ import java.util.Arrays;
  * A[4] = 5
  * A[5] = 6
  * the function should return 60, as the product of triplet (2, 4, 5) is maximal.
- *
+ * <p>
  * Write an efficient algorithm for the following assumptions:
- *
+ * <p>
  * N is an integer within the range [3..100,000];
  * each element of array A is an integer within the range [−1,000..1,000].
  */
