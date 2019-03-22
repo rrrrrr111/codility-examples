681507b61acb3649b2fb7c0b4e8d625e547bb052
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-22 14:45:35 +0300

    TreeHeight codility solution

---
+++ codility-solutions/src/test/groovy/ru/roman/task/codility/a099_future_training/Solution1TreeHeightTest.groovy
@@ -0,0 +1,76 @@
+package ru.roman.task.codility.a099_future_training
+
+import groovy.transform.CompileStatic
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+@CompileStatic
+@Test
+class Solution1TreeHeightTest {
+    private Solution1TreeHeight subj = new Solution1TreeHeight()
+
+    @DataProvider
+    private Object[][] provider() {
+        [
+                [new Tree(5,
+                        new Tree(3,
+                                new Tree(20),
+                                new Tree(21)
+                        ),
+                        new Tree(10,
+                                new Tree(1),
+                                null
+                        )
+                ), 2],
+                [new Tree(15,
+                        new Tree(18,
+                                new Tree(20,
+                                        new Tree(22,
+                                                new Tree(24,
+                                                        new Tree(26,
+                                                                new Tree(28),
+                                                                null
+                                                        ),
+                                                        null
+                                                ),
+                                                null
+                                        ),
+                                        null
+                                ),
+                                null
+                        ),
+                        new Tree(11,
+                                null,
+                                new Tree(9,
+                                        null,
+                                        new Tree(7,
+                                                null,
+                                                new Tree(5,
+                                                        null,
+                                                        new Tree(3,
+                                                                null,
+                                                                new Tree(1,
+                                                                        null,
+                                                                        new Tree(1,
+                                                                                null,
+                                                                                new Tree(-1)
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                ), 8],
+                [null, 0],
+                [new Tree(0), 0],
+        ] as Object[][]
+    }
+
+    @Test(dataProvider = "provider")
+    void test(Tree value, int expected) {
+
+        def actual = subj.solution(value)
+        assert actual == expected
+    }
+}
