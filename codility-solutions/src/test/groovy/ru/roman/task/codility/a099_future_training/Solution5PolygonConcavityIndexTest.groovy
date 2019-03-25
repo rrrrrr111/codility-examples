a30cb9309201ab8d83d59434584e54575930782b
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-25 03:25:52 +0300

    PolygonConcavityIndex codility solution

---
+++ codility-solutions/src/test/groovy/ru/roman/task/codility/a099_future_training/Solution5PolygonConcavityIndexTest.groovy
@@ -0,0 +1,25 @@
+package ru.roman.task.codility.a099_future_training
+
+import groovy.transform.CompileStatic
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+@CompileStatic
+@Test
+class Solution5PolygonConcavityIndexTest {
+    private Solution5PolygonConcavityIndex subj = new Solution5PolygonConcavityIndex()
+
+    @DataProvider
+    private Object[][] provider() {
+        [
+                [[] as int[], 0],
+        ] as Object[][]
+    }
+
+    @Test(dataProvider = "provider")
+    void test(int[] value, int expected) {
+
+        def actual = subj.solution(value)
+        assert actual == expected
+    }
+}
