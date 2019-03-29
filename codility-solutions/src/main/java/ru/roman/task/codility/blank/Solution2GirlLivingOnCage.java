c56d9235fb1e0bae604b3a5c26d0873562a412e3
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-29 16:40:05 +0300

    GirlLivingOnCage codility solution

---
+++ codility-solutions/src/main/java/ru/roman/task/codility/blank/Solution2GirlLivingOnCage.java
@@ -0,0 +1,59 @@
+package ru.roman.task.codility.blank;
+
+/**
+ * The girl moves on points, she went from point (AX, AY) to point (BX, BY) turned right 90 degrees and goes straight, you need to determine the first point she will meet
+ */
+class Solution2GirlLivingOnCage {
+    public String solution(int AX, int AY, int BX, int BY) {
+        System.out.printf("On input: %s %s %s %s%n", AX, AY, BX, BY);
+
+        if (AX == BX && AY == BY)
+            throw new IllegalArgumentException();
+
+        // if parallel to one of axes
+        if (AX == BX)
+            if (AY < BY) return String.format("%d,%d", BX + 1, BY);
+            else return String.format("%d,%d", BX - 1, BY);
+        else if (AY == BY)
+            if (AX < BX) return String.format("%d,%d", BX, BY - 1);
+            else return String.format("%d,%d", BX, BY + 1);
+
+        // otherwise
+        int xDistance = (AX > 0) == (BX > 0) ? Math.abs(AX - BX) : Math.abs(AX) + Math.abs(BX);
+        int yDistance = (AY > 0) == (BY > 0) ? Math.abs(AY - BY) : Math.abs(AY) + Math.abs(BY);
+        int gcd = gcd(xDistance, yDistance);
+        xDistance /= gcd;
+        yDistance /= gcd;
+
+
+        if (AX < BX && AY < BY) return String.format("%d,%d", BX + yDistance, BY - xDistance);
+        if (AX < BX && BY < AY) return String.format("%d,%d", BX - yDistance, BY - xDistance);
+        if (BX < AX && AY < BY) return String.format("%d,%d", BX + yDistance, BY + xDistance);
+        if (BX < AX && BY < AY) return String.format("%d,%d", BX - yDistance, BY + xDistance);
+
+        throw new IllegalStateException();
+    }
+
+    private static int gcd(int a, int b) {
+        int res = 1;
+
+        for (; ; ) {
+            if (a == b) {
+                return res * a;
+            } else if ((a % 2 == 0) && (b % 2 == 0)) {
+                a /= 2;
+                b /= 2;
+                res *= 2;
+            } else if (a % 2 == 0) {
+                a /= 2;
+            } else if (b % 2 == 0) {
+                b /= 2;
+
+            } else if (a > b) {
+                a -= b;
+            } else {
+                b -= a;
+            }
+        }
+    }
+}
---
+++ codility-solutions/src/test/groovy/ru/roman/task/codility/blank/Solution2GirlLivingOnCageTest.groovy
@@ -0,0 +1,41 @@
+package ru.roman.task.codility.blank
+
+import groovy.transform.CompileStatic
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+@CompileStatic
+@Test
+class Solution2GirlLivingOnCageTest {
+    private Solution2GirlLivingOnCage subj = new Solution2GirlLivingOnCage()
+
+    @DataProvider
+    private Object[][] provider() {
+        [
+                [-1, 3, 4, 3, "4,2"],
+                [1, -4, -3, -4, "-3,-3"],
+                [12, -1, 12, 1, "13,1"],
+                [-4, 8, -4, 6, "-5,6"],
+                [-2, 2, 5, 1, "4,-6"],
+                [1, -2, 5, 1, "8,-3"],
+                [-5, -19, 13, 5, "17,2"],
+                [-3, -5, 0, 4, "3,3"],
+                [-3, -5, 3, 13, "6,12"],
+                [0, 0, 2, 2, "3,1"],
+                [0, 0, 3, 3, "4,2"],
+                [0, 0, 9, 9, "10,8"],
+                [0, 0, -9, -9, "-10,-8"],
+                [0, 0, 9, -9, "8,-10"],
+                [0, 0, -9, 9, "-8,10"],
+                [0, 0, 9, 3, "10,0"],
+                [-5, -19, 10, 1, "14,-2"],
+        ] as Object[][]
+    }
+
+    @Test(dataProvider = "provider")
+    void test(int value1, int value2, int value3, int value4, String expected) {
+
+        def actual = subj.solution(value1, value2, value3, value4)
+        assert actual == expected
+    }
+}
