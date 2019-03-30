f0c8f5be5d9b32a5fb858078e4f9b89cb12cb290
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-30 21:04:30 +0300

    FibonacciNumbers example

--- java-study/src/main/java/ru/roman/algo/math/FibonacciNumbersExample.java
+++ java-study/src/main/java/ru/roman/algo/math/FibonacciNumbersExample.java
@@ -12,21 +12,25 @@ import static ru.roman.algo.PrintUtil.printRange;
  * Fn = Fn-1 + Fn-2
  * <p>
  * with seed values F0 = 0 and F1 = 1.
+ * <p>
+ * https://www.ics.uci.edu/~eppstein/161/960109.html#dynprog
  */
 class FibonacciNumbersExample {
 
     public static void main(String[] args) {
 
-        int maxValue = 46;
-        int[] fibonacci = prepareFibonacciNumbers(maxValue);
+        int valueNum = 46;
+        int[] fibonacci = prepareFibs(valueNum);
 
-        System.out.printf("Nums     : %s%n", printRange(0, maxValue + 1, 10));
+        System.out.printf("Nums     : %s%n", printRange(0, valueNum + 1, 10));
         System.out.printf("Fibonacci: %s%n", printArray(fibonacci, 10));
     }
 
-    private static int[] prepareFibonacciNumbers(int maxValue) {
-        int max = maxValue + 1;
-        int[] result = new int[max];
+    /**
+     * O(n), space O(n)
+     */
+    private static int[] prepareFibs(int num) {
+        int[] result = new int[num + 1];  // 1 extra to handle case, num = 0
         result[1] = 1;
 
         for (int i = 2; i < result.length; i++) {
@@ -35,9 +39,83 @@ class FibonacciNumbersExample {
         return result;
     }
 
-    private static int getFibNum(int n) {
-        if (n <= 1)
-            return n;
-        return getFibNum(n - 1) + getFibNum(n - 2);
+    /**
+     * O(n^2), space O(1)
+     */
+    private static int fib1(int num) {
+        if (num <= 1)
+            return num;
+        return fib1(num - 1) + fib1(num - 2);
+    }
+
+    /**
+     * O(n), space O(1)
+     */
+    private static int fib2(int n) {
+        int a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+    /**
+     * O(n), space O(1), Matrix Method
+     */
+    private static int fib3(int n) {
+        int[][] F = new int[][]{{1, 1}, {1, 0}};
+        if (n == 0)
+            return 0;
+        power(F, n - 1);
+
+        return F[0][0];
+    }
+
+    private static void power(int[][] F, int n) {
+
+        int[][] M = new int[][]{{1, 1}, {1, 0}};
+        for (int i = 2; i <= n; i++)        // n - 1 times multiply the matrix to {{1,0},{0,1}}
+            multiply(F, M);
+    }
+
+    private static void multiply(int[][] F, int[][] M) {
+        F[0][0] = F[0][0] * M[0][0] + F[0][1] * M[1][0];
+        F[0][1] = F[0][0] * M[0][1] + F[0][1] * M[1][1];
+        F[1][0] = F[1][0] * M[0][0] + F[1][1] * M[1][0];
+        F[1][1] = F[1][0] * M[0][1] + F[1][1] * M[1][1];
+    }
+
+    /**
+     * Based on matrix equation
+     * O(log n)
+     */
+    private static int fib4(int num) {
+
+        int[] nums = new int[1_000];
+        if (num == 0)
+            return 0;
+
+        if (num == 1 || num == 2)
+            return (nums[num] = 1);
+        if (nums[num] != 0)
+            return nums[num];
+        int k = (num & 1) == 1 ? (num + 1) / 2 : num / 2;
+
+        nums[num] = (num & 1) == 1
+                ? fib4(k) * fib4(k) + fib4(k - 1) * fib4(k - 1)
+                : (2 * fib4(k - 1) + fib4(k)) * fib4(k);
+        return nums[num];
+    }
+
+    /**
+     * O(1), space O(1), by formula
+     */
+    private static int fib5(int n) {
+        double phi = (1 + Math.sqrt(5)) / 2;
+        return (int) Math.round(Math.pow(phi, n) / Math.sqrt(5));
     }
 }
