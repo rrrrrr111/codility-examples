e003c4a7c8a53fe91ffee92a6b443bb7612b4476
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-03-31 15:43:55 +0300

    KnapsackProblem example, Branch and Bound solution

--- codility-solutions/src/main/java/ru/roman/task/gfg/dp/GfgKnapsackProblem.java
+++ codility-solutions/src/main/java/ru/roman/task/gfg/dp/GfgKnapsackProblem.java
@@ -1,7 +1,10 @@
 package ru.roman.task.gfg.dp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -26,8 +29,11 @@ import java.util.List;
 class GfgKnapsackProblem {
 
     String test(int Iv[], int Iw[], int W) {
-        return alg0Recursive(Iv, Iw, W, Iv.length) + "";
+        //return alg0Recursive(Iv, Iw, W, Iv.length) + "";
         //return alg1Dp(Iv, Iw, W);
+
+
+        return alg3UsingBranchAndBound(W, Iv, Iw);
     }
 
     /**
@@ -90,4 +96,138 @@ class GfgKnapsackProblem {
         Collections.sort(items);
         return result + " " + items;
     }
+
+    private String alg3UsingBranchAndBound(int W, int[] Iv, int[] Iw) {
+
+        Item[] arr = new Item[Iv.length];
+        int n = arr.length;
+
+        for (int i = 0; i < Iv.length; i++) {
+            arr[i] = new Item();
+            arr[i].value = Iv[i];
+            arr[i].weight = Iw[i];
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            double r1 = (double) a.value / a.weight;
+            double r2 = (double) b.value / b.weight;
+            return Double.compare(r1, r2);
+        });
+
+        // make a queue for traversing the node
+        Deque<Node> queue = new LinkedList<>();
+        Node u = new Node(), v = new Node();
+
+        // dummy node at starting
+        u.level = -1;
+        u.profit = u.weight = 0;
+        queue.push(u);
+
+        // One by one extract an item from decision tree
+        // compute profit of all children of extracted item
+        // and keep saving maxProfit
+        float maxProfit = 0;
+        while (!queue.isEmpty()) {
+            u = queue.getFirst();
+            queue.pop();
+
+            // If it is starting node, assign level 0
+            if (u.level == -1)
+                v.level = 0;
+
+            // If there is nothing on next level
+            if (u.level == n - 1)
+                continue;
+
+            // Else if not last node, then increment level,
+            // and compute profit of children nodes.
+            v.level = u.level + 1;
+
+            // Taking current level's item add current
+            // level's weight and value to node u's
+            // weight and value
+            v.weight = u.weight + arr[v.level].weight;
+            v.profit = u.profit + arr[v.level].value;
+
+            // If cumulated weight is less than W and
+            // profit is greater than previous profit,
+            // update maxprofit
+            if (v.weight <= W && v.profit > maxProfit)
+                maxProfit = v.profit;
+
+            // Get the upper bound on profit to decide
+            // whether to add v to queue or not.
+            v.bound = bound(v, n, W, arr);
+
+            // If bound value is greater than profit,
+            // then only push into queue for further
+            // consideration
+            if (v.bound > maxProfit)
+                queue.push(v);
+
+            // Do the same thing, but Without taking
+            // the item in knapsack
+            v.weight = u.weight;
+            v.profit = u.profit;
+            v.bound = bound(v, n, W, arr);
+            if (v.bound > maxProfit)
+                queue.push(v);
+        }
+
+        return String.valueOf(maxProfit);
+    }
+
+    // Returns bound of profit in subtree rooted with u.
+// This function mainly uses Greedy solution to find
+// an upper bound on maximum profit.
+
+    private float bound(Node u, int n, int W, Item arr[]) {
+        // if weight overcomes the knapsack capacity, return
+        // 0 as expected bound
+        if (u.weight >= W)
+            return 0;
+
+        // initialize bound on profit by current profit
+        float profit_bound = u.profit;
+
+        // start including items from index 1 more to current
+        // item index
+        int j = u.level + 1;
+        float totweight = u.weight;
+
+        // checking index condition and knapsack capacity
+        // condition
+        while ((j < n) && (totweight + arr[j].weight <= W)) {
+            totweight += arr[j].weight;
+            profit_bound += arr[j].value;
+            j++;
+        }
+
+        // If k is not n, include last item partially for
+        // upper bound on profit
+        if (j < n)
+            profit_bound += (W - totweight) * arr[j].value /
+                    arr[j].weight;
+
+        return profit_bound;
+    }
+
+    private static class Item {
+        float weight;
+        int value;
+
+    }
+    private static class Node {
+        // level --> Level of node in decision tree (or index
+        //			 in arr[]
+        // profit --> Profit of nodes on path from root to this
+        //		 node (including this node)
+        // bound ---> Upper bound of maximum profit in subtree
+        //		 of this node/
+        int level;
+        float profit, bound;
+
+        float weight;
+    }
+
 }
