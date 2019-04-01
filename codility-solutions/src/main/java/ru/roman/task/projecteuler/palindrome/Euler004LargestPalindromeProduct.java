f0145355210ccb51591a052d28c5ff275faf5b74
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-04-01 23:25:41 +0300

    Euler task 004

--- codility-solutions/src/main/java/ru/roman/task/projecteuler/palindrome/Euler004LargestPalindromeProduct.java
+++ codility-solutions/src/main/java/ru/roman/task/projecteuler/palindrome/Euler004LargestPalindromeProduct.java
@@ -9,7 +9,27 @@ public class Euler004LargestPalindromeProduct {
 
     public static void main(String[] args) {
 
-        int res = 0;
+        int res = -1;
+        for (int i = 100; i < 1000; i++) {
+            for (int j = 100; j < 1000; j++) {
+                int m = i * j;
+                if (isPalindrome(m) && m > res)
+                    res = m;
+            }
+        }
         System.out.println(res);
+    }
+
+
+    private static String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    private static boolean isPalindrome(String s) {
+        return s.equals(reverse(s));
+    }
+
+    private static boolean isPalindrome(int x) {
+        return isPalindrome(Integer.toString(x));
     }
 }
