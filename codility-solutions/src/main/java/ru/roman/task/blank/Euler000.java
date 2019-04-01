7b8c372797ae2c27340ced066f6fbdf4d540fbcd
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-04-01 23:41:14 +0300

    Euler task 005

--- codility-solutions/src/main/java/ru/roman/task/blank/Euler000.java
+++ codility-solutions/src/main/java/ru/roman/task/blank/Euler000.java
@@ -5,7 +5,7 @@ package ru.roman.task.blank;
  */
 public class Euler000 {
 
-    public static void main(String[] args) {
+    public static void main(String[] a) {
 
         int res = 0;
         System.out.println(res);
