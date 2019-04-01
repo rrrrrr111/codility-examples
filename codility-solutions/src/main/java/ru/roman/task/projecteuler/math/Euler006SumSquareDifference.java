785d8e58ba84e87a9d62876210e0d9c94f9c634c
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-04-01 23:49:27 +0300

    Euler task 006

---
+++ codility-solutions/src/main/java/ru/roman/task/projecteuler/math/Euler006SumSquareDifference.java
@@ -0,0 +1,28 @@
+package ru.roman.task.projecteuler.math;
+
+/**
+ * The sum of the squares of the first ten natural numbers is,
+ * <p>
+ * 12 + 22 + ... + 102 = 385
+ * The square of the sum of the first ten natural numbers is,
+ * <p>
+ * (1 + 2 + ... + 10)2 = 552 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ * <p>
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+public class Euler006SumSquareDifference {
+
+    public static void main(String[] a) {
+
+        int sum = 0;
+        int sum2 = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+            sum2 += i * i;
+        }
+        int res = sum * sum - sum2;
+
+        System.out.println(res);
+    }
+}
