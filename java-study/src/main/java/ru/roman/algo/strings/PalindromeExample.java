36db640dbf872d847a8a98f924e0cd6a37afd3dd
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-04-01 23:22:03 +0300

    Palindrome example

--- java-study/src/main/java/ru/roman/algo/strings/PalindromeExample.java
+++ java-study/src/main/java/ru/roman/algo/strings/PalindromeExample.java
@@ -6,6 +6,28 @@ package ru.roman.algo.strings;
 public class PalindromeExample {
 
     public static void main(String[] args) {
-        
+
+        print(80677608);
+        print(113323311);
+        print(806776080);
+    }
+
+    private static void print(int num) {
+        boolean res = isPalindrome(num);
+        System.out.printf("Num %s is palindrome: %s", num, res);
+        System.out.println();
+    }
+
+
+    private static String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    private static boolean isPalindrome(String s) {
+        return s.equals(reverse(s));
+    }
+
+    private static boolean isPalindrome(int x) {
+        return isPalindrome(Integer.toString(x));
     }
 }
