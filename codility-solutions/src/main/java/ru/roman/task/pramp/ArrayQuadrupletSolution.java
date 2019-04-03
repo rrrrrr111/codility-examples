bc111a6ef9d2929106c04a37b906e15b4958289d
rrrrrr111 <curdes@gmail.com>
2019-04-03 19:51:47 +0300

    ArrayQuadruplet Pramp Solution

--- codility-solutions/src/main/java/ru/roman/task/pramp/ArrayQuadrupletSolution.java
+++ codility-solutions/src/main/java/ru/roman/task/pramp/ArrayQuadrupletSolution.java
@@ -3,7 +3,33 @@ package ru.roman.task.pramp;
 import java.util.Arrays;
 
 /**
- *
+ * Array Quadruplet
+ * Given an unsorted array of integers arr and a number s, write a function findArrayQuadruplet that finds four numbers (quadruplet) in arr that sum up to s. Your function should return an array of these numbers in an ascending order. If such a quadruplet doesn’t exist, return an empty array.
+ * <p>
+ * Note that there may be more than one quadruplet in arr whose sum is s. You’re asked to return the first one you encounter (considering the results are sorted).
+ * <p>
+ * Explain and code the most efficient solution possible, and analyze its time and space complexities.
+ * <p>
+ * Example:
+ * <p>
+ * input:  arr = [2, 7, 4, 0, 9, 5, 1, 3], s = 20
+ * <p>
+ * output: [0, 4, 7, 9] # The ordered quadruplet of (7, 4, 0, 9)
+ * # whose sum is 20. Notice that there
+ * # are two other quadruplets whose sum is 20:
+ * # (7, 9, 1, 3) and (2, 4, 9, 5), but again you’re
+ * # asked to return the just one quadruplet (in an
+ * # ascending order)
+ * Constraints:
+ * <p>
+ * [time limit] 5000ms
+ * <p>
+ * [input] array.integer arr
+ * <p>
+ * 1 ≤ arr.length ≤ 100
+ * [input] integer s
+ * <p>
+ * [output] array.integer
  */
 class ArrayQuadrupletSolution {
 
