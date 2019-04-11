77050d791cea997233d92f1f523c4f66c9ec4464
rrrrrr111 <curdes@gmail.com>
2019-04-11 13:03:57 +0300

    Devskiller Faker

--- codility-solutions/src/main/java/ru/roman/task/devskiller/faker/Faker.java
+++ codility-solutions/src/main/java/ru/roman/task/devskiller/faker/Faker.java
@@ -1,31 +1,43 @@
 package ru.roman.task.devskiller.faker;
 
+
 import java.util.Random;
 
 class Faker {
 
+    private static Random RANDOM = new Random();
+    private static char[] ALL_CHARS = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
     public String letterify(String letterString) {
-        return bothify(letterString);
+        if (letterString == null || letterString.length() == 0) {
+            return letterString;
+        }
+
+        String result = letterString;
+        while (result.indexOf('?') > -1) {
+
+            String character = String.valueOf(ALL_CHARS[RANDOM.nextInt(ALL_CHARS.length)]);
+            result = result.replaceFirst("\\?", character);
+        }
+        return result;
     }
 
     public String numerify(String numberString) {
-        return bothify(numberString);
+        if (numberString == null || numberString.length() == 0) {
+            return numberString;
+        }
+        String result = numberString;
+        while (result.indexOf('#') > -1) {
+
+            String digit = String.valueOf(RANDOM.nextInt(10));
+            result = result.replaceFirst("#", digit);
+        }
+        return result;
     }
 
     public String bothify(String string) {
-        if (string == null || string.length() == 0) {
-            return string;
-        }
-        Random random = new Random();
-        String chars = "abcdefghijklmnopqrstuvwxyz";
 
-        while (string.indexOf('?') > -1 || string.indexOf('#') > -1) {
-            String ch = String.valueOf(chars.charAt(random.nextInt(chars.length())));
-            String digit = random.nextInt(10) + "";
-
-            string = string.replaceFirst("\\?", ch);
-            string = string.replaceFirst("#", digit);
-        }
-        return string;
+        String result = letterify(string);
+        return numerify(result);
     }
 }
