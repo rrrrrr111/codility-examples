0e129c7d4f9f9ced0bd8fac4b6244c0f6c5dc9e2
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2019-05-15 18:35:08 +0300

    MatrixSpiralCopy pramp solution

---
+++ codility-solutions/src/main/java/ru/roman/task/pramp/MatrixSpiralCopy.java
@@ -0,0 +1,81 @@
+package ru.roman.task.pramp;
+
+import java.util.Arrays;
+
+/**
+ * Given a 2D array (matrix) inputMatrix of integers, create a function spiralCopy that copies inputMatrix’s values into a 1D array in a spiral order, clockwise. Your function then should return that array. Analyze the time and space complexities of your solution.
+ * <p>
+ * Example:
+ * <pre>
+ * input:  inputMatrix  = [ [1,    2,   3,  4,    5],
+ *                          [6,    7,   8,  9,   10],
+ *                          [11,  12,  13,  14,  15],
+ *                          [16,  17,  18,  19,  20] ]</pre>
+ * <p>
+ * output: [1, 2, 3, 4, 5, 10, 15, 20, 19, 18, 17, 16, 11, 6, 7, 8, 9, 14, 13, 12]
+ * See the illustration below to understand better what a clockwise spiral order looks like.
+ * <p>
+ * alt Clockwise spiral order
+ * <p>
+ * Constraints:
+ * <p>
+ * [time limit] 5000ms
+ * <p>
+ * [input] array.array.integer inputMatrix
+ * <p>
+ * 1 ≤ inputMatrix[0].length ≤ 100
+ * 1 ≤ inputMatrix.length ≤ 100
+ * [output] array.integer
+ */
+class MatrixSpiralCopy {
+    public int[] solution(int[][] arr) {
+        System.out.printf("On input: %s\n", Arrays.deepToString(arr));
+
+        int w = arr[0].length, h = arr.length;
+        int[] res = new int[w * h];
+        int x = 0, y = 0, r = 0;
+
+        for (int l = 0; l < w; l++)
+            res[r++] = arr[0][l];
+        x = x + w - 1;
+
+        Boolean forward = null, down = true;
+        while (0 < w && 0 < h) {
+            if (down == Boolean.TRUE)
+                for (int l = y + 1; l < y + h; l++)
+                    res[r++] = arr[l][x];
+            if (down == Boolean.FALSE)
+                for (int l = y - 1; l > y - h; l--)
+                    res[r++] = arr[l][x];
+            if (forward == Boolean.TRUE)
+                for (int l = x + 1; l < x + w; l++)
+                    res[r++] = arr[y][l];
+            if (forward == Boolean.FALSE)
+                for (int l = x - 1; l > x - w; l--)
+                    res[r++] = arr[y][l];
+
+            if (down == Boolean.TRUE) {
+                down = null;
+                forward = false;
+                y = y + h - 1;
+                h--;
+            } else if (forward == Boolean.FALSE) {
+                forward = null;
+                down = false;
+                x = x - w + 1;
+                w--;
+            } else if (down == Boolean.FALSE) {
+                down = null;
+                forward = true;
+                y = y - h + 1;
+                h--;
+            } else if (forward == Boolean.TRUE) {
+                forward = null;
+                down = true;
+                x = x + w - 1;
+                w--;
+            }
+        }
+        return res;
+    }
+}
---
+++ codility-solutions/src/test/groovy/ru/roman/task/pramp/MatrixSpiralCopyTest.groovy
@@ -0,0 +1,38 @@
+package ru.roman.task.pramp
+
+import groovy.transform.CompileStatic
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+@CompileStatic
+@Test
+class MatrixSpiralCopyTest {
+    private MatrixSpiralCopy subj = new MatrixSpiralCopy()
+
+    @DataProvider
+    private Object[][] provider() {
+        [
+                [[
+                         [1, 2, 3, 4] as int[],
+                         [14, 15, 16, 5] as int[],
+                         [13, 20, 17, 6] as int[],
+                         [12, 19, 18, 7] as int[],
+                         [11, 10, 9, 8] as int[],
+                 ] as int[][], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] as int[]],
+                [[
+                         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as int[],
+                         [22, 23, 24, 25, 26, 27, 28, 29, 30, 11] as int[],
+                         [21, 20, 19, 18, 17, 16, 15, 14, 13, 12] as int[],
+                 ] as int[][], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                                16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30] as int[]],
+
+        ] as Object[][]
+    }
+
+    @Test(dataProvider = "provider")
+    void test(int[][] value, int[] expected) {
+
+        def actual = subj.solution(value)
+        assert actual == expected
+    }
+}
