bf3d2ee3143295b3594aa28d57ec528de2d17082
Roman Churganov <rchurganov@usetech.ru>
2023-08-18 20:57:43 +0300

    up

---
+++ kotlin-study/src/main/kotlin/ru/roman/kotlin/certification/algo/conviniencies/Comparators.kt
@@ -0,0 +1,14 @@
+package ru.roman.kotlin.certification.algo.conviniencies
+
+import java.util.*
+
+fun main() {
+
+
+    // reverse order heap
+    val map1 = TreeMap<Int, Any>(compareBy { it })
+    val map2 = TreeMap<Int, Any>(compareByDescending { it })
+
+
+
+}
