3d78c53db6a6f8e5d40f876fdd42b9faec36e3bf
Roman Churganov <rchurganov@usetech.ru>
2023-08-27 11:23:26 +0300

    up

--- kotlin-study/src/main/kotlin/ru/roman/kotlin/certification/algo/conviniencies/Binary.kt
+++ kotlin-study/src/main/kotlin/ru/roman/kotlin/certification/algo/conviniencies/Binary.kt
@@ -7,6 +7,8 @@ fun main() {
     // bit string
     val s1 = java.lang.Long.toBinaryString(v1)  // 101101101111111
     val s2 = java.lang.Long.toBinaryString(v2)  // 1111111111111111111111111111111111111111111111111100101110010001
+    val s3 = v2.toString(radix = 2)             // kotlin makes negative bin string                 -11010001101111
+    val s4 = v2.toULong().toString(radix = 2)   // 1111111111111111111111111111111111111111111111111100101110010001
 
     // add leading zeros
     java.lang.Long.toBinaryString(v1)
@@ -18,6 +20,10 @@ fun main() {
     //  and kind of completely inverted, so to return original decimal use [Long.parseUnsignedLong(..)],
     //  pad with zeros in case of positive value
     java.lang.Long.parseUnsignedLong(s2.padStart(64, '0'), 2) // -13423
+    // or we can invert all bits, add 1 and negate
+    java.lang.Long.parseLong(
+        String(s2.map { if (it == '0') '1' else '0' }.toCharArray()), 2
+    ) * -1 - 1 // -13423
 
 
 }
