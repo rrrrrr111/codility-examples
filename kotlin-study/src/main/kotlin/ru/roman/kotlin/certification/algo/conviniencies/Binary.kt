f57de3b3aecf18aaf5e3f032c7e165184a518fff
Roman Churganov <rchurganov@usetech.ru>
2023-08-27 16:48:24 +0300

    up

--- kotlin-study/src/main/kotlin/ru/roman/kotlin/certification/algo/conviniencies/Binary.kt
+++ kotlin-study/src/main/kotlin/ru/roman/kotlin/certification/algo/conviniencies/Binary.kt
@@ -5,12 +5,21 @@ package ru.roman.kotlin.certification.algo.conviniencies
  *  LSb - least significant bit, low-order bit
  *  MSb - most significant bit, high-order bit
  *
+ * Kotlin Bitwise Operations
+ *  Conjunction (and)	            a & b	    a and b
+ *  Disjunction (or)	            a | b	    a or b
+ *  Exclusive disjunction (xor)	    a ^ b	    a xor b
+ *  Inversion	                    ~ a	        a.inv()
+ *  Shift Left	                    a << bits   a shl bits
+ *  Shift Right	                    a >> bits   a shr bits
+ *  Unsigned Shift Right	        a >>> bits  a ushr bits
+ *
  */
 fun main() {
     var v1 = 23423L
     var v2 = -13423L
 
-    // bit string
+    // Int => string of bits
     val s1 = java.lang.Long.toBinaryString(v1)  // 101101101111111
     val s2 = java.lang.Long.toBinaryString(v2)  // 1111111111111111111111111111111111111111111111111100101110010001
     val s3 = v2.toString(radix = 2)             // kotlin makes negative bin string                 -11010001101111
@@ -31,5 +40,9 @@ fun main() {
         String(s2.map { if (it == '0') '1' else '0' }.toCharArray()), 2
     ) * -1 - 1 // -13423
 
+    // to read certain bit
+    //  - move target bit to LSb position, and if (num & 1) == 1 then LSb is 1 otherwise LSb is 0
 
+    // to check all bits are 1
+    //  - if (num & -1) == -1 then all bits are 1
 }
