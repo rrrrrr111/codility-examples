765285ef4bc6f3ddf625942ecec74966d40d6460
Roman Churganov <rchurganov@usetech.ru>
2023-08-28 21:13:13 +0300

    up

--- kotlin-study/src/main/kotlin/ru/roman/kotlin/certification/algo/conviniencies/Queue.kt
+++ kotlin-study/src/main/kotlin/ru/roman/kotlin/certification/algo/conviniencies/Queue.kt
@@ -2,11 +2,13 @@ package ru.roman.kotlin.certification.algo.conviniencies
 
 import java.util.Deque
 import java.util.LinkedList
+import java.util.Queue
 
 fun main() {
 
     // init
     val deque: Deque<Int> = LinkedList<Int>()
+    val deque1: Queue<Int> = LinkedList<Int>()
 
     deque.push(1)    // addFirst( .. )
     deque.poll()        // pollFirst()
